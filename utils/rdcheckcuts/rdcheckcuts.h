db366815bcfdde87339be2c953fcf4d4c39d8037
Fred Gleason <fredg@paraelsystems.com>
2018-08-15 20:22:15 +0000

    2018-08-15 Fred Gleason <fredg@paravelsystems.com> 	* Remved 'rdcheckcuts.pro'.

--- utils/rdcheckcuts/rdcheckcuts.h
+++ utils/rdcheckcuts/rdcheckcuts.h
@@ -2,7 +2,7 @@
 //
 // Check Rivendell Cuts for Valid Audio
 //
-//   (C) Copyright 2012,2016-2018 Fred Gleason <fredg@paravelsystems.com>
+//   (C) Copyright 2012-2018 Fred Gleason <fredg@paravelsystems.com>
 //
 //   This program is free software; you can redistribute it and/or modify
 //   it under the terms of the GNU General Public License version 2 as
