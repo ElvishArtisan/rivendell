82794caa92aeae4a7bc87ccaeb61719d3b7b7a58
Patrick Linstruth <patrick@deltecent.com>
2018-10-16 09:40:41 -0700

    Add LogError()

--- utils/rdimport/rdimport.h
+++ utils/rdimport/rdimport.h
@@ -73,6 +73,7 @@ class MainObject : public QObject
   void WriteTimestampCache(const QString &filename,const QDateTime &dt);
   bool SchedulerCodeExists(const QString &code) const;
   void ReadXmlFile(const QString &basename,RDWaveData *wavedata) const;
+  void LogError(RDConfig::LogPriority prio,const QString &msg);
   void SendNotification(RDNotification::Action action,unsigned cartnum);
   unsigned import_file_key;
   RDGroup *import_group;
