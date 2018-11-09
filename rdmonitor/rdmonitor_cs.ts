85a7704854ae247df6ec815f3c011e43e8cad03d
Fred Gleason <fredg@paraelsystems.com>
2018-11-09 08:15:02 -0500

    2018-11-09 Fred Gleason <fredg@paravelsystems.com> 	* Removed superceded 'StatusTip' class from rdmonitor(1).

--- rdmonitor/rdmonitor_cs.ts
+++ rdmonitor/rdmonitor_cs.ts
@@ -40,19 +40,19 @@
     <name>QObject</name>
     <message>
         <source>Status: OK</source>
-        <translation>Stav: OK</translation>
+        <translation type="obsolete">Stav: OK</translation>
     </message>
     <message>
         <source>Audio Store: FAILED</source>
-        <translation>Uložení zvuku: NEPODAŘILO SE</translation>
+        <translation type="obsolete">Uložení zvuku: NEPODAŘILO SE</translation>
     </message>
     <message>
         <source>Database: CONNECTION FAILED</source>
-        <translation>Databáze: SPOJENÍ SE NEZDAŘILO</translation>
+        <translation type="obsolete">Databáze: SPOJENÍ SE NEZDAŘILO</translation>
     </message>
     <message>
         <source>Database: SCHEMA SKEWED</source>
-        <translation>Databáze: SCHÉMA ZKRESLENO</translation>
+        <translation type="obsolete">Databáze: SCHÉMA ZKRESLENO</translation>
     </message>
 </context>
 </TS>
--- rdmonitor/rdmonitor_pt_BR.ts
+++ rdmonitor/rdmonitor_pt_BR.ts
@@ -36,23 +36,4 @@
         <translation type="unfinished"></translation>
     </message>
 </context>
-<context>
-    <name>QObject</name>
-    <message>
-        <source>Status: OK</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Audio Store: FAILED</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Database: CONNECTION FAILED</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Database: SCHEMA SKEWED</source>
-        <translation type="unfinished"></translation>
-    </message>
-</context>
 </TS>
