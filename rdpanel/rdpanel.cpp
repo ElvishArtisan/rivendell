d58fa84afea487b8f9f9037cd91cea1040c9162e
Patrick Linstruth <patrick@deltecent.com>
2018-11-09 07:35:12 -0800

    Fixed a regression in rdpanel(1) background image.

--- rdpanel/rdpanel.cpp
+++ rdpanel/rdpanel.cpp
@@ -53,8 +53,7 @@ RDCartDialog *panel_cart_dialog;
 MainWidget::MainWidget(QWidget *parent)
   : QWidget(parent)
 {
-  QPixmap *pm;
-  QPainter *pd;
+  QPixmap panel_skin_pixmap;
   QString err_msg;
 
   //
@@ -110,14 +109,13 @@ MainWidget::MainWidget(QWidget *parent)
   //
   // Allocate Global Resources
   //
-  panel_skin_pixmap=new QPixmap(rda->panelConf()->skinPath());
-  if(panel_skin_pixmap->isNull()||(panel_skin_pixmap->width()<1024)||
-     (panel_skin_pixmap->height()<738)) {
-    delete panel_skin_pixmap;
-    panel_skin_pixmap=NULL;
-  }
-  else {
-    setErasePixmap(*panel_skin_pixmap);
+  panel_skin_pixmap=QPixmap(rda->panelConf()->skinPath());
+  if(!panel_skin_pixmap.isNull()&&(panel_skin_pixmap.width()>=1024)&&
+     (panel_skin_pixmap.height()>=738)) {
+    QPalette p=palette();
+    p.setBrush(backgroundRole(),panel_skin_pixmap);
+    setPalette(p);
+    fprintf(stderr,"setPalette\n");
   }
 
   //
@@ -168,6 +166,7 @@ MainWidget::MainWidget(QWidget *parent)
     panel_panel->setLogfile(rda->config()->airplayLogname());
     panel_panel->setGeometry(10,10,panel_panel->sizeHint().width(),
 			 panel_panel->sizeHint().height());
+#if 0
     if(panel_skin_pixmap!=NULL) {
       pm=new QPixmap(1024,738);
       pd=new QPainter(pm);
@@ -177,6 +176,7 @@ MainWidget::MainWidget(QWidget *parent)
       delete pd;
       delete pm;
     }
+#endif
     panel_panel->setPauseEnabled(rda->panelConf()->panelPauseEnabled());
     panel_panel->setCard(0,rda->panelConf()->card(RDAirPlayConf::SoundPanel1Channel));
     panel_panel->setPort(0,rda->panelConf()->port(RDAirPlayConf::SoundPanel1Channel));
