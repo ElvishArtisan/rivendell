043a078cc7e73f44c5565bc99b93aee7ea975dc5
Fred Gleason <fredg@paraelsystems.com>
2020-02-14 18:09:15 -0500

    2020-02-14 Fred Gleason <fredg@paravelsystems.com> 	* Cleaned up the device access mode in the BroadcastTools 16x1 	switcher driver.

--- ripcd/bt16x1.cpp
+++ ripcd/bt16x1.cpp
@@ -2,7 +2,7 @@
 //
 // A Rivendell switcher driver for the BroadcastTools 16x1
 //
-//   (C) Copyright 2002-2019 Fred Gleason <fredg@paravelsystems.com>
+//   (C) Copyright 2002-2020 Fred Gleason <fredg@paravelsystems.com>
 //
 //   This program is free software; you can redistribute it and/or modify
 //   it under the terms of the GNU General Public License version 2 as
@@ -44,7 +44,7 @@ Bt16x1::Bt16x1(RDMatrix *matrix,QObject *parent)
     bt_device->setSpeed(tty->baudRate());
     bt_device->setWordLength(tty->dataBits());
     bt_device->setParity(tty->parity());
-    bt_device->open(QIODevice::Unbuffered|QIODevice::ReadWrite);
+    bt_device->open(QIODevice::Unbuffered|QIODevice::WriteOnly);
   }
   delete tty;
 }
