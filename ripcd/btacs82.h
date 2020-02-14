5c65965f156694a5b6cafaca85b3014aa69de090
Fred Gleason <fredg@paraelsystems.com>
2020-02-14 18:15:11 -0500

    2020-02-14 Fred Gleason <fredg@paravelsystems.com> 	* Fixed a regression in the BroadcastTools ACS8.2 switcher driver 	that could cause deadlocks and intermittent operation.

--- ripcd/btacs82.h
+++ ripcd/btacs82.h
@@ -2,7 +2,7 @@
 //
 // A Rivendell switcher driver for the BroadcastTools ACS 8.2
 //
-//   (C) Copyright 2002-2008,2016 Fred Gleason <fredg@paravelsystems.com>
+//   (C) Copyright 2002-2020 Fred Gleason <fredg@paravelsystems.com>
 //
 //   This program is free software; you can redistribute it and/or modify
 //   it under the terms of the GNU General Public License version 2 as
@@ -30,7 +30,6 @@
 #include <switcher.h>
 
 #define BTACS82_UNIT_ID 0
-#define BTACS82_POLL_INTERVAL 100
 #define BTACS82_GPIO_PINS 16
 
 class BtAcs82 : public Switcher
