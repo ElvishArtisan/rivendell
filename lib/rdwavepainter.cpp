eb231539f60ec03b232e472d9d48e9533bdcbb57
Fred Gleason <fredg@paravelsystems.com>
2020-12-26 20:58:18 -0500

    2020-12-26 Fred Gleason <fredg@paravelsystems.com> 	* Removed dependency on 'Q3PointArray' from 'RDWavePainter'.
    Signed-off-by: Fred Gleason <fredg@paravelsystems.com>

--- lib/rdwavepainter.cpp
+++ lib/rdwavepainter.cpp
@@ -2,7 +2,7 @@
 //
 // A Painter Class for Drawing Audio Waveforms
 //
-//   (C) Copyright 2002-2005,2016 Fred Gleason <fredg@paravelsystems.com>
+//   (C) Copyright 2002-2020 Fred Gleason <fredg@paravelsystems.com>
 //
 //   This program is free software; you can redistribute it and/or modify
 //   it under the terms of the GNU General Public License version 2 as
@@ -124,7 +124,7 @@ void RDWavePainter::drawWaveBySamples(int x,int w,int startsamp,int endsamp,
   resetXForm();
   setPen(color);
   setBrush(color);
-  Q3PointArray array(w+2);
+  QPolygon array(w+2);
   array.setPoint(0,0,center);
   array.setPoint(w+1,w+1,center);
   switch(effective_channel) {
