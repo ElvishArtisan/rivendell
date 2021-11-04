0b5a5e780704a5cea5212abdd5035474033909d7
Fred Gleason <fredg@paravelsystems.com>
2021-11-04 19:36:53 -0400

    2021-11-04 Fred Gleason <fredg@paravelsystems.com> 	* Added a 'rivwebpyapi' API.
    Signed-off-by: Fred Gleason <fredg@paravelsystems.com>

---
+++ apis/rivwebpyapi/api/rivwebpyapi.py
@@ -0,0 +1,69 @@
+# pypad.py
+#
+# Python binding for the Rivendell Web API
+#
+#   (C) Copyright 2021 Fred Gleason <fredg@paravelsystems.com>
+#
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License version 2 as
+#   published by the Free Software Foundation.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public
+#   License along with this program; if not, write to the Free Software
+#   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#
+
+import requests
+import untangle
+
+class RivWebPyApi(object):
+    """
+       Create a 'RivWebPyApi' object for accessing the Web API.
+    """
+
+    def __init__(self,url,username,passwd):
+        """
+           Initialize a WebAPI connection to a Rivendell instance.
+
+           Takes three arguments:
+
+           url - The URL of the service. Typically of the form
+                'http://rivendell.example.com/rd-bin/rdxport.cgi' (string).
+
+           username - The Rivendell Username to connect as (string).
+
+           password - The password for the specified Rivendell Username
+                      (string).
+        """
+        self.__connection_url=url
+        self.__connection_username=username
+        self.__connection_password=passwd
+
+    def ListServices(self,trackable):
+        """
+          Returns a list of Rivendell services (Element).
+
+          Takes one argument:
+
+          trackable - Boolean. If True, return will include only services that
+                      are configured to support voice tracking. If False,
+                      all configured services will be included.
+        """
+        trackarg='0'
+        if(trackable):
+            trackarg='1'
+        postdata={
+            'COMMAND': '21',
+            'LOGIN_NAME': self.__connection_username,
+            'PASSWORD': self.__connection_password,
+            'TRACKABLE': trackarg
+        }
+        r=requests.post(self.__connection_url,data=postdata)
+        if(r.status_code!=requests.codes.ok):
+            r.raise_for_status()
+        return untangle.parse(r.text)
---
+++ apis/rivwebpyapi/tests/list_services.py
@@ -0,0 +1,61 @@
+#!%PYTHON_BANGPATH%
+
+# login_test.py
+#
+# RivWebPyApi test script for Rivendell
+#
+#  Attempt to log in to the Rivendell Web API
+#
+#   (C) Copyright 2021 Fred Gleason <fredg@paravelsystems.com>
+#
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License version 2 as
+#   published by the Free Software Foundation.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public
+#   License along with this program; if not, write to the Free Software
+#   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#
+
+import getpass
+import rivwebpyapi
+import sys
+
+url='';
+username=''
+password=''
+
+#
+# Get login parameters
+#
+usage='login_test --url=<rd-url> --username=<rd-username>'
+for arg in sys.argv:
+    f0=arg.split('=')
+    if(len(f0)==2):
+        if(f0[0]=='--url'):
+            url=f0[1]
+        if(f0[0]=='--username'):
+            username=f0[1]
+        if(f0[0]=='--password'):
+            password=f0[1]
+if(not password):
+    password=getpass.getpass()
+if((not url)or(not username)):
+    print(usage)
+    sys.exit(1)
+
+
+webapi=rivwebpyapi.RivWebPyApi(url=url,username=username,passwd=password)
+services=webapi.ListServices(False)
+for svc in services.serviceList.service:
+    print('NAME: '+svc.name.cdata)
+    if(svc.description.cdata):
+        print('DESCRIPTION: '+svc.description.cdata)
+    else:
+        print('DESCRIPTION: [none]')
+    print('')
