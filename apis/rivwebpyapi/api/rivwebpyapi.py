0615903a32a5c4ecf3015f6b8822677751b1c4b1
Fred Gleason <fredg@paravelsystems.com>
2021-12-06 16:23:50 -0500

    2021-12-06 Fred Gleason <fredg@paravelsystems.com> 	* Added an 'AssignSchedCode()' method to the 'rivwebpyapi' API.
    Signed-off-by: Fred Gleason <fredg@paravelsystems.com>

--- apis/rivwebpyapi/api/rivwebpyapi.py
+++ apis/rivwebpyapi/api/rivwebpyapi.py
@@ -416,6 +416,41 @@ class rivwebpyapi(object):
         if(r.status_code!=requests.codes.ok):
             r.raise_for_status()
 
+    def AssignSchedCode(self,cart_number,sched_code):
+        """
+          Assigned a schedule code to a cart.
+
+          Takes the following arguments:
+
+          cart_number - The number of the desired cart, in the range
+                        1 - 999999 (integer)
+
+          sched_code - An existing schedule code (integer).
+        """
+
+        if((cart_number<1)or(cart_number>999999)):
+            raise ValueError('invalid cart number')
+        if(not sched_code):
+            raise ValueError('invalid schedule code')
+
+        #
+        # Build the WebAPI arguments
+        #
+        postdata={
+            'COMMAND': '25',
+            'LOGIN_NAME': self.__connection_username,
+            'PASSWORD': self.__connection_password,
+            'CART_NUMBER': str(cart_number),
+            'CODE': sched_code
+        }
+
+        #
+        # Fetch the XML
+        #
+        r=requests.post(self.__connection_url,data=postdata)
+        if(r.status_code!=requests.codes.ok):
+            r.raise_for_status()
+
     def DeleteLog(self,log_name):
         """
           Delete an existing log.
