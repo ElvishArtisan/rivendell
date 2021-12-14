732c7300fa63e3ff569307c2b6a46b511cbd2471
Fred Gleason <fredg@paravelsystems.com>
2021-12-14 15:38:14 -0500

    2021-12-14 Fred Gleason <fredg@paravelsystems.com> 	* Updated the comment headers of files below 'apis/rivwebpyapi/' to 	reflecting licensing under the GNU Lesser General Public License v2 	rather than the GNU General Public License version 2.
    Signed-off-by: Fred Gleason <fredg@paravelsystems.com>

--- apis/rivwebpyapi/tests/lock_log.py
+++ apis/rivwebpyapi/tests/lock_log.py
@@ -9,15 +9,15 @@
 #   (C) Copyright 2021 Fred Gleason <fredg@paravelsystems.com>
 #
 #   This program is free software; you can redistribute it and/or modify
-#   it under the terms of the GNU General Public License version 2 as
+#   it under the terms of the GNU Lesser General Public License version 2 as
 #   published by the Free Software Foundation.
 #
 #   This program is distributed in the hope that it will be useful,
 #   but WITHOUT ANY WARRANTY; without even the implied warranty of
 #   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#   GNU General Public License for more details.
+#   GNU Lesser General Public License for more details.
 #
-#   You should have received a copy of the GNU General Public
+#   You should have received a copy of the GNU Lesser General Public
 #   License along with this program; if not, write to the Free Software
 #   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 #
