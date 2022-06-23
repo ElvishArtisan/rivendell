613dcc2b466e2120cc4d3f988c6e9dea8742b6c5
Fred Gleason <fredg@paravelsystems.com>
2022-06-23 13:22:50 -0400

    2022-06-23 Fred Gleason <fredg@paravelsystems.com> 	* Changed the default '--process-interval=' value from 60 seconds 	to 30 seconds in rdrssd(8).
    Signed-off-by: Fred Gleason <fredg@paravelsystems.com>

--- rdrssd/rdrssd.h
+++ rdrssd/rdrssd.h
@@ -2,7 +2,7 @@
 //
 // Rivendell RSS Processor Service
 //
-//   (C) Copyright 2020 Fred Gleason <fredg@paravelsystems.com>
+//   (C) Copyright 2020-2022 Fred Gleason <fredg@paravelsystems.com>
 //
 //   This program is free software; you can redistribute it and/or modify
 //   it under the terms of the GNU General Public License version 2 as
@@ -24,7 +24,7 @@
 #include <QObject>
 #include <QTimer>
 
-#define RDRSSD_DEFAULT_PROCESS_INTERVAL 60000
+#define RDRSSD_DEFAULT_PROCESS_INTERVAL 30000
 #define RDRSSD_USAGE "[--process-interval=<secs>]\n\n"
 
 class MainObject : public QObject
