acc5e2d73f53146b0c87e21783057b53c91a47ea
Fred Gleason <fredg@paravelsystems.com>
2023-01-20 11:06:40 -0500

    2023-01-20 Fred Gleason <fredg@paravelsystems.com> 	* Fixed a regression in rdairplay(1) that broke the 'Audition Head' 	and 'Audition Tail' buttons in the Full Log widget.
    Signed-off-by: Fred Gleason <fredg@paravelsystems.com>

--- lib/rdlogplay.cpp
+++ lib/rdlogplay.cpp
@@ -132,8 +132,7 @@ RDLogPlay::RDLogPlay(int id,RDEventPlayer *player,QObject *parent)
   //
   play_audition_line=-1;
   if((rda->station()->cueCard()>=0)&&
-     (rda->station()->cuePort()>=0)&&
-     isWidgetType()) {
+     (rda->station()->cuePort()>=0)) {
     play_audition_player=
       new RDSimplePlayer(play_cae,rda->ripc(),rda->station()->cueCard(),
 			 rda->station()->cuePort(),0,0);
