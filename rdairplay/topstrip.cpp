dde4191bfeba800f7c895b2ba759b3380d494441
Fred Gleason <fredg@paravelsystems.com>
2023-04-29 14:07:24 -0400

    2023-04-29 Fred Gleason <fredg@paravelsystems.com> 	* Changed the behavior of the logo display in rdairplay(1) so as to 	show the default Rivendell logo when the 'Logo Image' control in 	rdadmin(1) is not set to a valid graphical image file.
    Signed-off-by: Fred Gleason <fredg@paravelsystems.com>

--- rdairplay/topstrip.cpp
+++ rdairplay/topstrip.cpp
@@ -62,16 +62,12 @@ TopStrip::TopStrip(QWidget *parent)
   // Logo
   //
   QPixmap pix(rda->airplayConf()->logoPath());
+  if(pix.isNull()) {
+    pix.load("/usr/share/pixmaps/rivendell/rdairplay_logo.png");
+  }
   d_logo=new QLabel(this);
   d_logo->setAlignment(Qt::AlignCenter);
   d_logo->setPixmap(pix);
-  /*
-  d_logo->setFont(bannerFont());
-  d_logo->setText(QString::asprintf("User Logo\n(%dx%d)",
-					    RD_RDAIRPLAY_LOGO_WIDTH,
-					    RD_RDAIRPLAY_LOGO_HEIGHT));
-  d_logo->setStyleSheet("background-color: #99FF99");
-  */
 
   setFocusPolicy(Qt::NoFocus);
 }
