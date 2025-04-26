ba1c230402b4e64346931b006dabef911c00dc1f
Fred Gleason <fredg@paravelsystems.com>
2025-04-26 18:48:10 -0400

    2025-04-26 Fred Gleason <fredg@paravelsystems.com> 	* Cleaned up compiler warnings in 'web/webget/'.
    Signed-off-by: Fred Gleason <fredg@paravelsystems.com>

--- web/webget/webget.cpp
+++ web/webget/webget.cpp
@@ -2,7 +2,7 @@
 //
 // Rivendell upload/download utility
 //
-//   (C) Copyright 2018-2023 Fred Gleason <fredg@paravelsystems.com>
+//   (C) Copyright 2018-2025 Fred Gleason <fredg@paravelsystems.com>
 //
 //   This program is free software; you can redistribute it and/or modify
 //   it under the terms of the GNU General Public License version 2 as
@@ -416,8 +416,8 @@ void MainObject::PutAudio()
   //
   // Generate Title
   //
-  QStringList f0=filename.split("/",QString::SkipEmptyParts);
-  QStringList f1=f0.last().split(".",QString::KeepEmptyParts);
+  QStringList f0=filename.split("/",Qt::SkipEmptyParts);
+  QStringList f1=f0.last().split(".",Qt::KeepEmptyParts);
   QString short_name=f0.last();
   f1.removeLast();
   QString title=f1.join(".");
@@ -789,7 +789,7 @@ void MainObject::SaveSourceFile(const QString &filepath) const
     return;
   }
   QDateTime now=QDateTime::currentDateTime();
-  QStringList f0=filepath.split("/",QString::SkipEmptyParts);
+  QStringList f0=filepath.split("/",Qt::SkipEmptyParts);
   QString filename=rda->config()->saveWebgetFilesDirectory()+"/"+
     now.toString("yyyyMMdd-hhmmss-")+f0.last();
 
@@ -816,7 +816,7 @@ void MainObject::SaveSourceFile(const QString &filepath) const
       return;
     }
     filename=rda->config()->saveWebgetFilesDirectory()+"/"+
-      now.toString("yyyyMMdd-hhmmss")+QString().sprintf("[%d]-",num)+f0.last();
+      now.toString("yyyyMMdd-hhmmss")+QString::asprintf("[%d]-",num)+f0.last();
     num++;
   }
 
